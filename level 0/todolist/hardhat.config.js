b93978b535718aa1a47b5f218c05d9dffea17f84
subo <www.suso@qq.com>
2025-01-21 12:32:52 +0800

    代码提交

---
+++ level 0/todolist/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox"); 
+require("dotenv").config(); 
+/** @type import('hardhat/config').HardhatUserConfig */ 
+module.exports = { 
+solidity: "0.8.27", 
+networks: { 
+hardhat: {}, 
+sepolia: { 
+url:
+ "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY, accounts: [`0x${process.env.PRIVATE_KEY}`], 
+ }, 
+ }, 
+ };
